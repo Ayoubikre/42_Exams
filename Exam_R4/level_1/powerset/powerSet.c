b8484b20be5028c2ba84f7dcda72fd4907a1456e
Ayoub Akritah <aakritah@c2r5p9.1337.ma>
2025-10-03 21:46:01 +0100

    42_Exams

--- Exam_R4/level_1/powerset/powerSet.c
+++ Exam_R4/level_1/powerset/powerSet.c
@@ -6,7 +6,7 @@
 /*   By: aakritah <aakritah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 13:12:44 by mlaffita          #+#    #+#             */
-/*   Updated: 2025/05/07 12:07:02 by aakritah         ###   ########.fr       */
+/*   Updated: 2025/10/03 21:45:18 by aakritah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,58 +30,52 @@ int ft_sum(int *t2, int x)
     return sum;
 }
 
-void ft_solve(int n , int *t, int *t2, int *lock, int s, int x, int i)
+void ft_solve(int n, int *t, int *t2, int *lock, int s, int x, int i, int sum)
 {
-    if(ft_sum(t2,x)==n)
+    if (sum == n)
     {
-        int j=0;
-        while(j<x)
+        if (x != 0) 
         {
-            printf("%d ", t2[j]);
-            j++;
+            for (int j = 0; j < x; j++)
+                printf("%d ", t2[j]);
+            printf("\n");
         }
-                printf("\n");
+    }
 
-        return ;
-    }  
-
-    while(i<s)
+    while (i < s)
     {
-        if(lock[i])
+        if (lock[i])
         {
             i++;
             continue;
         }
-        lock[i]=1;
-            t2[x]=t[i];
-            ft_solve(n , t, t2, lock, s, x+1, i+1);
-        lock[i]=0;
-        i++;
-    }  
-}
-
-int main(int ac, char**ar)
-{
-    if(ac<2)
-        return 1;
-    
-    int s=ac-2;
-    int *t=calloc(s, sizeof(int ));
-    int *t2=calloc(s, sizeof(int ));
-    int *lock=calloc(s, sizeof(int ));
-    if(!t || !t || !lock)
-        return 1;
-    int i=1;
-    int n=atoi(ar[i]);
-    i++;
-    int j=0;
-    while(i<ac)
-    {
-        t[j]=atoi(ar[i]);
-        j++;
+        lock[i] = 1;
+            t2[x] = t[i];
+            ft_solve(n, t, t2, lock, s, x + 1, i + 1, sum + t[i]);
+        lock[i] = 0;
         i++;
     }
-    ft_solve(n , t, t2, lock, s, 0,0);
+}
+
+int main(int ac, char **ar)
+{
+    if (ac < 2)
+        return 1;
+
+    int s = ac - 2;
+    int *t = calloc(s, sizeof(int));
+    int *t2 = calloc(s, sizeof(int));
+    int *lock = calloc(s, sizeof(int));
+    if (!t || !t2 || !lock)
+        return 1;
+    int n = atoi(ar[1]);
+    for (int j = 0; j < s; j++)
+        t[j] = atoi(ar[j + 2]);
+
+    ft_solve(n, t, t2, lock, s, 0, 0, 0);
+
     free(t);
+    free(t2);
+    free(lock);
     return 0;
 }
