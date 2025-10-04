62e1c2da4aaba3e822474b71dece477961976da3
Ayoub Akritah <aakritah@c2r1p1.1337.ma>
2025-10-04 18:02:33 +0100

    42_Exams

--- Exam_R4/level_2/ft_popen/ft_popen.c
+++ Exam_R4/level_2/ft_popen/ft_popen.c
@@ -1,51 +1,57 @@
-#include <unistd.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_popen.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakritah <aakritah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/01 15:14:09 by aakritah          #+#    #+#             */
+/*   Updated: 2025/10/04 14:08:21 by aakritah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+									// Mine:
 
 int	ft_popen(const char *file, char *const argv[], char type)
 {
-	if (!file || !argv || (type != 'r' && type != 'w'))
-		return -1;
+	pid_t	pid;
+	int		fd[2];
 
-
-
-	int fd[2];
-	pipe(fd);
-
-
-	
+	if(!file || !argv || (type != 'w' && type != 'r'))
+        return -1;
+        
+	if (pipe(fd) == -1)
+		return (-1);
+        
+	pid = fork();
+	if (pid < 0)
+		return (-1);
+	else if (pid == 0)
+	{
+		if (type == 'r'){            
+			if(dup2(fd[1], STDOUT_FILENO) == -1)
+                exit(-1);
+        }
+		else if (type == 'w'){
+			if(dup2(fd[0],STDIN_FILENO) == -1)
+                exit(-1);
+        }
+        close(fd[0]);
+        close(fd[1]);
+		if (execvp(file, argv) == -1)
+			exit(-1);
+	}
 	if (type == 'r')
-	{
-		if (fork() == 0)
-		{
-			dup2(fd[1], 1);
-			close(fd[0]);
-			close(fd[1]);
-			execvp(file, argv);
-			exit (-1);
-		}
-		close(fd[1]);
-		return (fd[0]);
-	}
-	if (type == 'w')
-	{
-		if (fork() == 0)
-		{
-			dup2(fd[0], 0);
-			close(fd[0]);
-			close(fd[1]);
-			execvp(file, argv);
-			exit (-1);
-		}
-		close(fd[0]);
-		return (fd[1]);
-	}
-	return -1;
+        return (close(fd[1]), fd[0]);
+    return (close(fd[0]), fd[1]);
 }
 
 int main() 
 {
     printf("fd : %d\n", ft_popen("ls", (char *const[]){"ls", NULL}, 'r'));
     printf("fd : %d\n", ft_popen("ls", (char *const[]){"ls", NULL}, 'w'));
-    // while(1){}
 }
--- Exam_R4/level_2/picoshell/picoshell.c
+++ Exam_R4/level_2/picoshell/picoshell.c
@@ -1,63 +1,83 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   picoshell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakritah <aakritah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/04 14:09:51 by aakritah          #+#    #+#             */
+/*   Updated: 2025/10/04 16:23:33 by aakritah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+									// Mine:
+
 #include <stdio.h>
-#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-// int picoshell(char **cmds[])
-// {
-//     int res = 0;
-//     int i = 0;
-//     int prv_fd = -1;
-//     int st;
-//     int fd[2];
-//     pid_t pid;
+int picoshell(char **cmds[])
+{
+    if (!cmds)
+        return -1;
     
+    int s=0;
+    while(cmds[s])
+        s++;
     
-//     while (cmds[i])
-//     {
-//         if(cmds[i + 1])
-//             pipe(fd);
-//         pid = fork();
-//         if(pid == -1)
-//             return 1;
+    int t[s-1][2];
+    for(int i=0; i<s-1; i++)
+        if(pipe(t[i])==-1)
+            return -1;
+    
+    int i=0;
+    pid_t pid;
+    while(i<s)
+    {
+        pid=fork();
+        if(pid==-1)
+            exit(1);
+        else if(pid==0)
+        {
+            if(i==0)
+                dup2(t[i][1], 1);
+            else if(i==s-1)
+                dup2(t[i-1][0],0);
+            else{
+                dup2(t[i-1][0], 0);
+                dup2(t[i][1], 1);
+            }
             
-//         if (pid == 0)
-//         {
-//             if(prv_fd != -1)
-//             {
-//                 dup2(prv_fd, 0);
-//                 close(prv_fd);
-//             }
-//             if(cmds[i + 1])
-//             {
-//                 dup2(fd[1], 1);
-//                 close(fd[1]);
-//             }
-//             execvp(cmds[i][0], cmds[i]);
-//             return 1;
-//         }
-    
-//         while (wait(&st) == -1)
-//         {
-//             if(WIFEXITED(st) && WEXITSTATUS(st) == 0)
-//                 res = 1;
-//         }
-//         if(prv_fd != -1)
-//             close(prv_fd);
-//         if(cmds[i + 1])
-//         {
-//             close(fd[1]);
-//             prv_fd = fd[0];
-//         }
-//         i++;
-//     }
-//     return res;
-// }
+            for(int i=0; i<s-1; i++){
+                close(t[i][0]);    
+                close(t[i][1]);    
+            }
+            execvp(cmds[i][0], cmds[i]);
+            exit(-1);
+        }
+        i++; 
+    }
 
-// int main()
-// {
-//     char *c[2] = {"ls", NULL};
-//     char **cmds[] = {c, NULL};
-//     printf("<%d>\n", picoshell(cmds));
-// }
+    for(int i=0; i<s-1; i++){
+        close(t[i][0]);    
+        close(t[i][1]);    
+    }
+
+    for(int i=0; i<s; i++)
+        wait(NULL);
+    
+    return 0;
+}
+
+int main()
+{
+    char *cmd1[] = {"echo", "squalala!", NULL};
+    char *cmd2[] = {"cat", NULL};
+    char *cmd3[] = {"sed", "s/a/b/g", NULL};
+
+    // char **t[] = {cmd2, cmd3, NULL};
+    char **t[] = {cmd1, cmd2, cmd3, NULL};
+    picoshell(t);
+
+    return 0;
+}
